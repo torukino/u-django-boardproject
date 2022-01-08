ca7690faaff82ada5b1860ba90273fc9c226ff98
torukino <toruk0131@gmail.com>
2022-01-09 03:12:33 +0900

    init

---
+++ boardapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import signupfunc, loginfunc
+
+urlpatterns = [
+    path('signup/', signupfunc, name='signup'),
+    path('login/', loginfunc, name='login'),
+]
---
+++ boardapp/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+from django.db import IntegrityError
+
+def signupfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.create_user(username, '', password)
+            return render(request, 'signup.html', {'some':100})
+        except IntegrityError:
+            return render(request, 'signup.html', {'error':'このユーザはすでに登録されています'})
+
+    return render(request, 'signup.html', {'some':100})
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'login.html', {'context':'logged in'}) 
+        else:
+            return render(request, 'login.html', {'context':'not logged in'})
+
+    return render(request, 'login.html', {'context':'get method'}) 
+        
