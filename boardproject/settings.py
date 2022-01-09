04e052ad67494abfcdc8aca44dfc7327f884c638
torukino <toruk0131@gmail.com>
2022-01-09 23:35:21 +0900

    社内SNS

--- boardproject/settings.py
+++ boardproject/settings.py
@@ -116,9 +116,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'sta/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = 'medi/'
+
+LOGIN_URL = 'login/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
